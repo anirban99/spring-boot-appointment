40e465ef4983d74dc131167aa73d2917e9110371
Anirban <canirban99@gmail.com>
2019-01-21 23:34:28 +0100

    Initial Commit

---
+++ src/main/java/com/example/appointment/SpringBootAppointmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppointmentApplication.class, args);
+	}
+
+}
+
